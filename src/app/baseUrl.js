dca73d69ab1f595087e27b7c541d2f68288f3d47
SougataMondal2000 <sougataofficial362000@gmail.com>
2025-01-24 11:17:09 +0530

    fix

--- src/app/baseUrl.js
+++ src/app/baseUrl.js
@@ -1,2 +1,2 @@
-// export const baseURL = "https://configstaging.trainright.fit";
-export const baseURL = "http://localhost:7009";
+export const baseURL = "https://configstaging.trainright.fit";
+// export const baseURL = "http://localhost:7009";
